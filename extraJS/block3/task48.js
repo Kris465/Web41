04e530cdc4be55ee579b3cd6798359a14480cdf0
Kris465 <nataza7394@gmail.com>
2025-03-13 20:48:54 +0300

    task48

---
+++ extraJS/block3/task48.js
@@ -0,0 +1,21 @@
+function calculateLogicalExpressions() {
+    const possibleValues = [true, false];
+  
+    for (const X of possibleValues) {
+      for (const Y of possibleValues) {
+        console.log(`X = ${X}, Y = ${Y}`);
+  
+        const a = !(X && !Y) || X;
+        console.log(`  a) !(X && !Y) || X: ${a}`);
+
+        const b = (Y && !X) || !Y;
+        console.log(`  b) (Y && !X) || !Y: ${b}`);
+  
+        const c = (!Y && !X) || Y;
+        console.log(`  c) (!Y && !X) || Y: ${c}`);
+  
+        console.log("---");
+      }
+    }
+  }
+  
