13c8b23fa2a1088ad3e40d3a1ad9cdb3fa2beb80
Kris465 <nataza7394@gmail.com>
2025-03-18 20:50:07 +0300

    динозавреке

---
+++ extraJS/block3/task27.js
@@ -0,0 +1,24 @@
+let conditionD = !(x > 0 && x < 5);
+
+let conditionE = (x > 3) || (y < 5);
+
+let conditionF = (x > 10);
+
+let conditionG = (x > 3) && (x < 10);
+
+let conditionH = (y > 0) && (x < 20);
+
+let conditionI = (y > 0) && (x < 5);
+
+x = 4; 
+y = 5; 
+
+console.log(`Условие A: ${conditionA}`); 
+console.log(`Условие B: ${conditionB}`); 
+console.log(`Условие C: ${conditionC}`);
+console.log(`Условие D: ${conditionD}`); 
+console.log(`Условие E: ${conditionE}`);
+console.log(`Условие F: ${conditionF}`);
+console.log(`Условие G: ${conditionG}`);
+console.log(`Условие H: ${conditionH}`);
+console.log(`Условие I: ${conditionI}`);
