a64a7cf57615ee2d595d318305b16ab54328c315
Kris465 <nataza7394@gmail.com>
2025-04-08 19:44:01 +0300

    22

---
+++ extraPHP/block2/task22.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Определение десятков и сотен</title>
+</head>
+<body>
+    <h1>Введите натуральное число (n > 99)</h1>
+    <form method="post">
+        <input type="number" name="number" min="100" required>
+        <input type="submit" value="Отправить">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $inputNumber = $_POST['number'];
+
+        if ($inputNumber > 99) {
+            $hundreds = floor($inputNumber / 100) % 10;
+
+            $tens = floor($inputNumber / 10) % 10;
+
+            echo "<h2>Результаты:</h2>";
+            echo "Количество сотен: " . htmlspecialchars($hundreds) . "<br>";
+            echo "Количество десятков: " . htmlspecialchars($tens) . "<br>";
+        } else {
+            echo "Ошибка: Введите натуральное число больше 99.<br>";
+        }
+    }
+    ?>
+</body>
+</html>
