a135b73c5bfed8419032900dbd4e137b9d69b870
Kris465 <nataza7394@gmail.com>
2025-04-17 20:41:15 +0300

    task4 and 5

---
+++ extraJS/block5/task4.js
@@ -0,0 +1,3 @@
+for (let i = 10; i <= 25; i++) {
+    console.log(i + " " + (i + 0.4));
+}
---
+++ extraJS/block5/task5.js
@@ -0,0 +1,13 @@
+
+
+console.log("а)");
+for (let i = 21; i >= 10; i--) {
+    console.log(i + " " + (i - 1.8));
+}
+
+
+
+console.log("\nб)");
+for (let i = 45; i >= 25; i--) {
+    console.log(i + " " + (i - 0.5) + " " + (i - 0.8));
+}
