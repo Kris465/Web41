7ddfd5a31cfb255f35639095ea4cd84e4da21057
Kris465 <nataza7394@gmail.com>
2025-04-17 21:02:21 +0300

    task14

---
+++ extraJS/block5/task14.js
@@ -0,0 +1,8 @@
+console.log("Таблица умножения на 9");
+console.log("-------------------------");
+console.log("Множитель\tРезультат");
+
+for (let i = 1; i <= 10; i++) {
+    const result = 9 * i;
+    console.log(`${i}\t\t${result}`);
+}
