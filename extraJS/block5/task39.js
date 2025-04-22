ddf832254114effc97a537f77bf97bd8231ff574
Kris465 <nataza7394@gmail.com>
2025-04-22 19:54:54 +0300

    39

---
+++ extraJS/block5/task39.js
@@ -0,0 +1,7 @@
+
+const numbers = [1, 2, 10];
+
+
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+console.log("Сумма чисел:", sum);git 
--- extraJS/block5/task39.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$numbers = [1, 2, 10];
-
-$sum = array_sum($numbers);
-
-
-echo "Сумма чисел: " . $sum;
-?>
