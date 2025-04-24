cb947612767926911f13f7c32a4bf0f82ba5534b
Kris465 <nataza7394@gmail.com>
2025-04-24 20:56:32 +0300

    bylochki

---
+++ extraJS/block5/task89.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+
+const userInput = prompt("Введите шестизначное число:");
+
+if (userInput.length === 6 && !isNaN(userInput)) {
+    let sum = 0;
+
+    for (let i = 0; i < userInput.length; i++) {
+        sum += parseInt(userInput[i]);
+    }
+
+    console.log(`Сумма цифр числа ${userInput} равна ${sum}`);
+} else {
+    console.log("Пожалуйста, введите корректное шестизначное число.");
+}
