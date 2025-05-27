9a594619bc532634a8ed6d7e7fd41695298be456
Kris465 <nataza7394@gmail.com>
2025-05-27 19:16:51 +0300

    task54

---
+++ extraJS/block6/task54.js
@@ -0,0 +1,19 @@
+const prompt = require('prompt-sync')(); 
+
+
+const numberInput = prompt("Введите натуральное число:");
+const number = parseInt(numberInput, 10);
+
+if (isNaN(number) || number <= 0) {
+    console.log("Некорректный ввод");
+} else {
+    const digits = number.toString().split('');
+    let isNonDecreasingFromRight = true;
+    for (let i = 0; i < digits.length - 1; i++) {
+        if (parseInt(digits[i]) < parseInt(digits[i + 1])) {
+            isNonDecreasingFromRight = false;
+            break;
+        }
+    }
+    console.log(`Последовательность цифр числа при просмотре справа налево ${isNonDecreasingFromRight ? 'упорядочена по неубыванию' : 'не упорядочена по неубыванию'}`);
+}
