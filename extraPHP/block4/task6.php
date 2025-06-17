e826efa9e2ccab24a8b716a2a315b185c7f1805b
Kris465 <nataza7394@gmail.com>
2025-06-17 20:34:38 +0300

    Kirgiz2017

---
+++ extraPHP/block4/task6.php
@@ -0,0 +1,14 @@
+<?php
+$a = 5.3;
+$b = 3.7;
+if ($a > $b) {
+    echo "$a больше $b\n";
+} else {
+    echo "$b больше $a\n";
+}
+if ($a < $b) {
+    echo "$a меньше $b\n";
+} else {
+    echo "$b меньше $a\n";
+}
+?>
