fb26873200a323c51d90bd026f0ec5eef466224c
Kris465 <nataza7394@gmail.com>
2025-06-24 20:30:13 +0300

    task14

--- extraJS/block1/task14.js
+++ extraJS/block1/task14.js
@@ -1,5 +1,5 @@
 const prompt = require('prompt-sync')();
-
+let n = prompt('введите n:');     
 let x = prompt('введите x:');       
 let y = prompt('введите y:');       
 let alpha = prompt('введите alpha:');   
@@ -9,7 +9,7 @@ let result1 = 2 * x;
 let result2 = Math.sin(x);
 let result3 = Math.pow(a, 2);
 let result4 = Math.sqrt(x);
-let result5 = 5 * Math.cos(beta);
+let result5 = Math.abs(n);
 let result6 = alpha * Math.sqrt(2);
 let result7 = Math.sin(Math.cos(beta) + Math.sin(beta));
 let result8 = -7 * 5 * a * Math.pow(x, 2);
