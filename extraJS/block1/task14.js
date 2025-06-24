279c7cf4f4450289213b6ac4e0857310f253ea36
Kris465 <nataza7394@gmail.com>
2025-06-24 20:25:01 +0300

    task14

--- extraJS/block1/task14.js
+++ extraJS/block1/task14.js
@@ -1,14 +1,18 @@
 const prompt = require('prompt-sync')();
 
-let x = prompt('введите x: ');
-let sinx = prompt('введите sinx: ');
-let a = prompt('введите a:');
-let n = prompt('введите n: ');
-let sinA = prompt('введите sinA: ');
-let cosB = prompt('введите cosB: ');
-
-console.log(` a) 2x = ${2 * x}`);
-console.log(` б) sin x = ${Math.sin(x * Math.PI / 180)}`)
-console.log(` В) a^2 ${a * a}`)
-console.log(` г) sqrt(x) = ${Math.sqrt(x)}`)
-console.log(``)
+let x = prompt('введите x:');       
+let y = prompt('введите y:');       
+let alpha = prompt('введите alpha:');   
+let beta = prompt('введите beta:');   
+let a = prompt('введите a:');      
+let result1 = 2 * x;
+let result2 = Math.sin(x);
+let result3 = Math.pow(a, 2);
+let result4 = Math.sqrt(x);
+let result5 = 5 * Math.cos(beta);
+let result6 = alpha * Math.sqrt(2);
+let result7 = Math.sin(Math.cos(beta) + Math.sin(beta));
+let result8 = -7 * 5 * a * Math.pow(x, 2);
+let result9 = 3 * Math.sqrt(x + Math.sqrt(y));
+let result10 = Math.sin(2 * alpha) * Math.cos(beta);
+console.log(result1, result2, result3, result4, result5, result6, result7, result8, result9, result10);
