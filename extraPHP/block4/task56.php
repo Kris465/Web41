35d67475c99dae955bc27b7b1c0bc1b3f96b1176
Kris465 <nataza7394@gmail.com>
2025-07-01 21:07:50 +0300

    ymni chelovek v ochkax i s rykzakom foto ckachat

---
+++ extraPHP/block4/task56.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8" />
+<title>Проверка цифр в двузначном числе</title>
+</head>
+<body>
+<h2>Проверка цифр в двузначном числе</h2>
+<form method="post">
+Введите двузначное число: <input type="number" name="number" required><br><br>
+<input type="submit" value="Проверить">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$number = $_POST['number'];
+if (!preg_match('/^-?\d{2}$/', $number)) {
+    echo "<p>Пожалуйста, введите двузначное число.</p>";
+} else {
+    $abs_str = ltrim((string)$number, '-');
+    if (strpos($abs_str, '4') !== false || strpos($abs_str, '7') !== false) {
+        echo "<p>В числе есть цифра 4 или 7.</p>";
+    } else {
+        echo "<p>В числе нет цифр 4 и 7.</p>";
+    }
+    if (strpos($abs_str, '3') !== false || strpos($abs_str, '6') !== false || strpos($abs_str, '9') !== false) {
+        echo "<p>В числе есть цифра 3, 6 или 9.</p>";
+    } else {
+        echo "<p>В числе нет цифр 3, 6 и 9.</p>";
+    }
+}
+}
+?>
+</body>
+</html>
