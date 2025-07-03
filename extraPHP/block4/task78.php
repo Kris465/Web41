f6606bd04e95500621b812b5d409f1341f574d10
Kris465 <nataza7394@gmail.com>
2025-07-03 20:41:10 +0300

    azerbajan

---
+++ extraPHP/block4/task78.php
@@ -0,0 +1,15 @@
+<?php
+$a = intval(readline());
+$b = intval(readline());
+$c = intval(readline());
+
+if ($a % 2 == 0) {
+    echo $a . "\n";
+}
+if ($b % 2 == 0) {
+    echo $b . "\n";
+}
+if ($c % 2 == 0) {
+    echo $c . "\n";
+}
+?>
