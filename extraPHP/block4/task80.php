3296340cd23a03c7eb4afe3bf7cd459bb8e6a80d
Kris465 <nataza7394@gmail.com>
2025-07-03 20:52:52 +0300

    yan toples

---
+++ extraPHP/block4/task80.php
@@ -0,0 +1,25 @@
+<?php
+$a = floatval(readline());
+$b = floatval(readline());
+$c = floatval(readline());
+
+if ($a > 1.6 && $a < 3.8) {
+    echo $a . "\n";
+}
+if ($b > 1.6 && $b < 3.8) {
+    echo $b . "\n";
+}
+if ($c > 1.6 && $c < 3.8) {
+    echo $c . "\n";
+}
+
+if ($a > 0.7 && $a < 5.1) {
+    echo $a . "\n";
+}
+if ($b > 0.7 && $b < 5.1) {
+    echo $b . "\n";
+}
+if ($c > 0.7 && $c < 5.1) {
+    echo $c . "\n";
+}
+?>
